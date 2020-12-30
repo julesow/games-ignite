f314cc22b606e0df20d249aca33e584f2ad99d4c
sowsouleymane <souleymane.sow@eurostar.com>
2020-12-30 09:48:51 +0000

    adds further apis

--- src/api.js
+++ src/api.js
@@ -29,6 +29,10 @@ const lastYear = `${currentYear - 1}-${currentMonth }-${currentDay }`
 const nextYear = `${currentYear + 1}-${currentMonth }-${currentDay }`
 
 
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
+const upcomingGames = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`
 
-export const popularGamesURL = ()=>`${base_url}${popular_games}`
+export const popularGamesURL = ()=>`${base_url}${popularGames}`
+export const newGamesURL = ()=>`${base_url}${newGames}`
+export const upcomingGamesURL = ()=>`${base_url}${upcomingGames}`

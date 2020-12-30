73938d9e19ac5662062bac015e8a705f7b241bbe
sowsouleymane <souleymane.sow@eurostar.com>
2020-12-30 11:59:15 +0000

    adds Games components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,75 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+//redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../redux/actions/gamesActions";
 
+//styles and animations
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Game from "../components/Game";
 
 const Home = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { popular, newGames, upcoming } = useSelector(state => state.games);
 
-    useEffect(() => {
-      dispatch(loadGames());
-    });
-    return (
-        <div>
-            Home Page
-        </div>
-    )
-}
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+  return (
+    <GameList>
+      <h2>Upcoming games</h2>
+      <Games>
+        {upcoming.map(game => (
+          <Game
+            key={game.id}
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+      <h2>Popular games</h2>
+      <Games>
+        {popular.map(game => (
+          <Game
+            key={game.id}
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+      <h2>New games</h2>
+      <Games>
+        {newGames.map(game => (
+          <Game
+            key={game.id}
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+    </GameList>
+  );
+};
 
-export default Home
+const GameList = styled(motion.div)`
+    padding:0rem 5rem;
+    h2{
+        padding:5rem 0rem;
+    }
+
+`;
+const Games = styled(motion.div)`
+    min-height:80vh;
+    display:grid;
+    grid-template-columns:repeat(auto-fit,minmax(500px,1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap:5rem;
+`;
+
+export default Home;
